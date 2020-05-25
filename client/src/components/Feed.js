97a8c7ac86737e0805071d859a7472f02f749cfb
Martha <Arnaud>
2020-05-24 23:54:17 -0500

    added dummy imgs for feed

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Navbar from "./NavBar";
 import { Button } from 'react-bootstrap';
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
+import {faArrowDown} from "@fortawesome/free-solid-svg-icons";
+import {faCommentAlt} from "@fortawesome/free-regular-svg-icons";
+//import {corgi} from "../assets/corgi.png";
 
 class Feed extends React.Component {
 
@@ -11,17 +15,66 @@ class Feed extends React.Component {
                 <Navbar></Navbar>
                 <div className="container">
                     <div className="row">
-                        
                         <div className="col-sm-9 ml-5">
                             <h1 className="my-5">Memes Kencho</h1>
                             </div>
                             <span>
-                            <Button variant="success" style={{ height: 80, marginTop: 10 }}>Agregar meme</Button>{' '}
+                            <Button variant="success" style={{ height: 80, marginTop: 30 }}>Agregar meme</Button>{' '}
                             </span>
-                           
-                           
-                          
+
                         </div>
+                        <div className="imagesFeed">
+                               <div className="borderImage">
+                                   <p>@username</p>
+                               <br>
+                               </br>
+                                   <img className="images"src={require('../assets/corgi.png')} alt="Corgi"/>
+                                   <p>Caption del meme</p>
+                                   <br>
+                                    </br>
+                                   <div>
+                                       <span>
+                                   <FontAwesomeIcon icon={faArrowUp} size='4x' className="arrowUp">
+                                   </FontAwesomeIcon>
+                                   
+                                
+                                   <FontAwesomeIcon icon={faArrowDown} size='4x' className="arrowDown">
+                                   </FontAwesomeIcon>
+                                  
+                                    <span className="commentBox">
+                                   <FontAwesomeIcon icon={faCommentAlt} size='4x' className="comment">
+                                   </FontAwesomeIcon>
+                                   </span>
+                                   </span>
+                               </div>
+                               </div>
+                               <br>
+                               </br>
+                               <div className="borderImage">
+                               <br>
+                               </br>
+                                   <img className="images"src={require('../assets/golden.jpg')} alt="Corgi"/>
+                                   <p>Caption del meme</p>
+                                   <br>
+                                    </br>
+                                   <div>
+                                       <span>
+                                   <FontAwesomeIcon icon={faArrowUp} size='4x' className="arrowUp">
+                                   </FontAwesomeIcon>
+                                   
+                                
+                                   <FontAwesomeIcon icon={faArrowDown} size='4x' className="arrowDown">
+                                   </FontAwesomeIcon>
+                                  
+                                    <span className="commentBox">
+                                   <FontAwesomeIcon icon={faCommentAlt} size='4x' className="comment">
+                                   </FontAwesomeIcon>
+                                   </span>
+                                   </span>
+                               </div>
+                               </div>
+                               
+                           </div>
                     </div>
                 
             </>
